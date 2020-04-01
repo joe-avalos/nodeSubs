1139ffb6dcec23b6ee2b39ad71ecb60c89c8f4d7
jose <jose@cobbleweb.co.uk>
2020-04-01 13:55:22 -0600

    master - initial commit

---
+++ controllers/plans-controller.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+
+
+// GET /api/plans
+router.get('/', (req, res) => {
+
+})
+
+// GET /api/plans/:id
+router.get('/:id', (req, res) => {
+
+})
+
+// POST /api/plans
+router.post('/', (req, res) => {
+
+})
+
+// DELETE /api/plans/:id
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const PlansController = require('./controllers/plans-controller')
+
+const PORT = process.env.PORT = 3000
+
+const app = express()
+
+app.use('/api/plans', PlansController)
+
+app.listen(PORT, ()=>console.log(`Server listening on port ${PORT}`))
